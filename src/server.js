cb19f77c5b07cef3125ab8706448136153f60506
Junior Volonte <juniorvolontedev@gmail.com>
2023-02-06 06:40:46 -0300

    body params

--- src/server.js
+++ src/server.js
@@ -1,9 +1,12 @@
 const express = require("express");
 
 const app = express();
+app.use(express.json());
 
 app.post("/users", (req, res) => {
-  res.send("Você chamou o post");
+  const { name, email, password } = req.body;
+
+  res.json({ name, email, password });
 });
 
 const PORT = 3333;
