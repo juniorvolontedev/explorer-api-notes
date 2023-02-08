6f14be5bb19a2c8a202cdacfaa12ae308c414de9
Junior Volonte <juniorvolontedev@gmail.com>
2023-02-07 22:00:42 -0300

    listando tags

--- src/controllers/NotesController.js
+++ src/controllers/NotesController.js
@@ -79,7 +79,17 @@ class NotesController {
         .orderBy("title");
     }
 
-    return response.json(notes);
+    const userTags = await knex("tags").where({ user_id });
+    const notesWithTags = notes.map((note) => {
+      const noteTags = userTags.filter((tag) => tag.note_id === note.id);
+
+      return {
+        ...notes,
+        tags: noteTags,
+      };
+    });
+
+    return response.json(notesWithTags);
   }
 }
 
