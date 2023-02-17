8757dd7a139c8696ca684ba02dfc75f89b9cd4f5
Junior Volonte <juniorvolontedev@gmail.com>
2023-02-17 07:45:28 -0300

    users repository in memory

--- src/services/userCreateService.spec.js
+++ src/services/userCreateService.spec.js
@@ -1,4 +1,5 @@
 const UserCreateService = require("./UserCreateService");
+const UserRepositoryInMemory = require("../repositories/UserRepositoryInMemory");
 
 it("user should be create", async () => {
   const user = {
@@ -7,7 +8,8 @@ it("user should be create", async () => {
     password: "123",
   };
 
-  const userCreateService = new UserCreateService();
+  const userRepositoryInMemory = new UserRepositoryInMemory();
+  const userCreateService = new UserCreateService(userRepositoryInMemory);
   const userCreated = await userCreateService.execute(user);
 
   expect(userCreated).toHaveProperty("id");
